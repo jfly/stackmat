d39913f5840a0f84206e339e7a81397b01e883b2
Jeremy Fleischman <jeremyfleischman@gmail.com>
2014-01-27 21:04:45 -0800

    Dynamically computing correct urls now.

--- uploadsite/www/list.py
+++ uploadsite/www/list.py
@@ -1,17 +1,38 @@
 #!/usr/bin/env python
 
 import os
+import sys
 import json
+import urlparse
 import cgitb; cgitb.enable(format='text')
 
 import upload
 
 def main():
     clipsDir = upload.getClipsDir()
-    clips = os.listdir(clipsDir)
-    clips = filter(lambda f: f.endswith(".wav"), clips)
-    clips = map(lambda f: os.path.join(clipsDir, f), clips)
-    print json.dumps(clips)
+    files = os.listdir(clipsDir)
+    jsonClips = []
+    for f in files:
+        time_timestamp, extension = os.path.splitext(f)
+        if extension != ".wav":
+            continue
+        if "-" not in time_timestamp:
+            # We're only interested in files of the form TIME-TIMESTAMP.wav
+            continue
+        time, timestamp = time_timestamp.split("-")
+        timestamp = int(timestamp)
+        fullerPath = os.path.join(clipsDir, f)
+
+        scriptUri = os.environ["SCRIPT_URI"]
+        lastSlash = scriptUri.rfind("/")
+        parentUri = scriptUri[:lastSlash + 1]
+        url = parentUri + fullerPath
+        jsonClip = {}
+        jsonClip['url'] = url
+        jsonClip['time'] = time
+        jsonClip['timestamp'] = timestamp
+        jsonClips.append(jsonClip)
+    print json.dumps(jsonClips)
 
 if __name__ == "__main__":
     print "Content Type: application/json"
